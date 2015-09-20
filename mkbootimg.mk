82510ca872fdbb0a2681c29734d6c0f572544ac0
Rabih Salamey <somcom3@gmail.com>
2015-09-20 14:05:14 -0400

    clark: append dtb's to zImage
         * We don't need kinzie's dtbs included.

--- mkbootimg.mk
+++ mkbootimg.mk
@@ -1,8 +1,7 @@
 LOCAL_PATH := $(call my-dir)
 
-## Build and run dtbtool
-DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolOP2$(HOST_EXECUTABLE_SUFFIX)
-INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+KERNEL_CONFIG := $(KERNEL_OUT)/.config
+MSM8992_DTS_NAMES := msm8992
 
 ifneq ($(TARGET_KERNEL_ARCH),)
 KERNEL_ARCH := $(TARGET_KERNEL_ARCH)
@@ -10,11 +9,30 @@ else
 KERNEL_ARCH := $(TARGET_ARCH)
 endif
 
-$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
-	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
-	$(hide) $(DTBTOOL) -2 -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/dts/
-	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
+MSM8992_DTS_FILES = $(wildcard $(TOP)/$(TARGET_KERNEL_SOURCE)/arch/$(KERNEL_ARCH)/boot/dts/msm8992-clark*.dts)
+MSM8992_DTS_FILE = $(lastword $(subst /, ,$(1)))
+DTB_FILE = $(addprefix $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/,$(patsubst %.dts,%.dtb,$(call MSM8992_DTS_FILE,$(1))))
+ZIMG_FILE = $(addprefix $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/,$(patsubst %.dts,%-zImage,$(call MSM8992_DTS_FILE,$(1))))
+KERNEL_ZIMG = $(KERNEL_OUT)/arch/$(KERNEL_ARCH)/boot/zImage
 
+define append-msm8992-dtb
+mkdir -p $(KERNEL_OUT)/arch/arm/boot;\
+$(foreach MSM8992_DTS_NAME, $(MSM8992_DTS_NAMES), \
+   $(foreach d, $(MSM8992_DTS_FILES), \
+      $(DTC) -p 1024 -O dtb -o $(call DTB_FILE,$(d)) $(d); \
+      cat $(KERNEL_ZIMG) $(call DTB_FILE,$(d)) > $(call ZIMG_FILE,$(d));))
+endef
+
+## Build and run dtbtool
+DTBTOOL := $(HOST_OUT_EXECUTABLES)/dtbToolOP2$(HOST_EXECUTABLE_SUFFIX)
+INSTALLED_DTIMAGE_TARGET := $(PRODUCT_OUT)/dt.img
+
+$(INSTALLED_DTIMAGE_TARGET): $(DTBTOOL) $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr $(INSTALLED_KERNEL_TARGET)
+	@echo -e ${CL_CYN}"Start DT image: $@"${CL_RST}
+	$(call append-msm8992-dtb)
+	$(call pretty,"Target dt image: $(INSTALLED_DTIMAGE_TARGET)")
+	$(hide) $(DTBTOOL) -o $(INSTALLED_DTIMAGE_TARGET) -s $(BOARD_KERNEL_PAGESIZE) -p $(KERNEL_OUT)/scripts/dtc/ $(KERNEL_OUT)/arch/arm/boot/
+	@echo -e ${CL_CYN}"Made DT image: $@"${CL_RST}
 
 ## Overload bootimg generation: Same as the original, + --dt arg
 $(INSTALLED_BOOTIMAGE_TARGET): $(MKBOOTIMG) $(INTERNAL_BOOTIMAGE_FILES) $(INSTALLED_DTIMAGE_TARGET)
