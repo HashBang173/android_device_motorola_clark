904fce70aeac638de30a01d4b8cce852665dbb65
LuK1337 <priv.luk@gmail.com>
2015-09-20 11:30:41 -0400

    clark: typo on kernel cmdline for permissive selinux
        * It is permissive, not permisive.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,7 +50,7 @@ TARGET_USES_C2D_COMPOSITION := true
 
 # Kernel
 BOARD_CUSTOM_BOOTIMG_MK := device/motorola/clark/mkbootimg.mk
-BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 earlyprintk=msm_hsl_uart,0xf991e000 utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup androidboot.selinux=permisive
+BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 earlyprintk=msm_hsl_uart,0xf991e000 utags.blkdev=/dev/block/bootdevice/by-name/utags utags.backup=/dev/block/bootdevice/by-name/utagsBackup androidboot.selinux=permissive
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_OFFSET := 0x8000
