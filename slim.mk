71c2dd97fc7ae5f93c2d1c01baf379cae0c5fc9e
Griffin Millender <griffinn.millender@gmail.com>
2015-09-23 17:17:57 -0500

    Slim
    Change-Id: If316a200b3703ee5619465b004327753ee1f7032

--- slim.mk
+++ slim.mk
@@ -5,11 +5,11 @@ TARGET_SCREEN_WIDTH := 2560
 # Inherit 64-bit configs
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/slim/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/motorola/clark/aosp_clark.mk)
@@ -17,7 +17,7 @@ $(call inherit-product, device/motorola/clark/aosp_clark.mk)
 DEVICE_PACKAGE_OVERLAYS += device/motorola/clark/overlay
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_NAME := cm_clark
+PRODUCT_NAME := slim_clark
 PRODUCT_BRAND := Android
 PRODUCT_MODEL := XT1575
 
