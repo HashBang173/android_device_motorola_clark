a3b27e190e58b510d48645503930ad80f04a129e
Hashbang173 <hashbang173@gmail.com>
2019-06-16 14:22:13 -0400

    Remove extra camera packages
    Change-Id: Ic2bf36a61d49bb20e6abc5821abb89f43001a87b

--- device.mk
+++ device.mk
@@ -57,14 +57,6 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     camera.msm8992 \
-    libcamera \
-    libqomx_core \
-    libmm-qcamera \
-    libmmcamera_interface \
-    libmmjpeg_interface \
-    libshims_camera \
-    libshims_media \
-    mm-qcamera-app \
     Snap
 
 PRODUCT_COPY_FILES += \
